e0286e4a4c9032881aab0949adcdfd734becc7a4
BombMarley <kile146@gmail.com>
2023-04-24 18:55:56 +0200

    fix firestore queries, fix ordedby firestore queries, add delete transation button and function

--- src/pages/home/TransactionList.jsx
+++ src/pages/home/TransactionList.jsx
@@ -1,8 +1,11 @@
+import { useFirestore } from '../../hooks/useFirestore'
 
 // styles
 import styles from './Home.module.css'
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument, response } = useFirestore('transactions')
+  console.log(response);
     
   return (
     <ul className={styles.transactions}>
@@ -10,6 +13,7 @@ export default function TransactionList({ transactions }) {
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>X</button>
         </li>
       ))}
     </ul>
